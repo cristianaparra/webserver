7f5ba12867f881eca9533bcdf591e3c5d1c9b630
Cristian Parra <cristian.a.parra@gmail.com>
2021-02-11 13:08:16 -0300

    usando hbc, desplegando con dotenv

---
+++ app.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+const express = require("express");
+const hbs = require('hbs');
+
+const app = express();
+const port = process.env.PORT;
+//handlebars
+
+app.set('view engine', 'hbs'); 
+hbs.registerPartials(__dirname + '/views/partials');
+
+//servir contenido e
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.render("home", {
+    nombre: " Cristian Parra",
+    titulo: "Curso de Node",
+  });
+});
+
+
+
+app.get("/generic", (req, res) => {
+  res.render('generic',{
+    nombre: " Cristian Parra",
+    titulo: "Curso de Node",
+  });
+});
+app.get("/elements", (req, res) => {
+  res.render('elements',{
+    nombre: " Cristian Parra",
+    titulo: "Curso de Node",
+  });
+});
+
+app.get("/hola", function (req, res) {
+  res.send("wacho");
+});
+app.get("*", function (req, res) {
+  res.send("no existe");
+});
+
+app.listen(port);
